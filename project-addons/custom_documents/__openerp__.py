0c3e7cf644f0eedb646ee555542a0433e1e0c18f
jesusVMayor <jesus@pexego.es>
2015-02-12 19:55:28 +0100

    [MOD]custom_documents: Mejoras de documentos.

--- project-addons/custom_documents/__openerp__.py
+++ project-addons/custom_documents/__openerp__.py
@@ -34,6 +34,7 @@
              'views/report_purchase_order.xml',
              'views/report_header.xml', 'sale_view.xml', 'stock_view.xml',
              'views/report_invoice.xml',
-             'data/paperformat_data.xml'],
+             'data/paperformat_data.xml',
+             'payment_mode_view.xml'],
     "installable": True
 }
