d32984b35fec022d6abcb64abf6f9e4612ea56c5
jesusVMayor <jesus@comunitea.com>
2015-11-19 18:20:00 +0100

    [IMP]custom_documents: Añadido migrations para recalcular las columnas necesarias.

---
+++ project-addons/custom_documents/migrations/2.0/pre-migrate.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 Comunitea All Rights Reserved
+#    $Jesús Ventosinos Mayor <jesus@comunitea.com>$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+def migrate(cr, version):
+    print("Se inicia el borrado de las columnas a recalcular.")
+    cr.execute("""alter table stock_move drop column order_price_unit;""")
+    cr.execute("""alter table stock_pack_operation drop column price_subtotal;""")
+    cr.execute("""alter table stock_move_service drop column order_price_unit;""")
