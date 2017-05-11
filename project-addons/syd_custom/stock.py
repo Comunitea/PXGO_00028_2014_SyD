ac73649fc15f536b26d3ff6e3e24dcb61304b8f2
jesusVMayor <jesus@comunitea.com>
2017-05-11 16:50:00 +0200

    [IMP]syd_custom: Añadido campo margen en informe de ventas, añadido stock de producto en vista lotes.

--- project-addons/syd_custom/stock.py
+++ project-addons/syd_custom/stock.py
@@ -18,7 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from openerp import models, fields, api, exceptions, _
+from openerp import models, fields, api
 
 
 class StockPicking(models.Model):
@@ -60,3 +60,25 @@ class StockPicking(models.Model):
                 invoice_obj.write(write_vals)
 
         return invoice_ids
+
+
+class StockProductionLot(models.Model):
+
+    _inherit = 'stock.production.lot'
+
+    product_available = fields.Float('Quantity available',
+                                 compute='_compute_product_available',
+                                 search='_search_total')
+
+    @api.multi
+    def _compute_product_available(self):
+        for lot in self:
+            lot.product_available = lot.product_id.qty_available
+
+    @api.model
+    def _search_total(self, operator, operand):
+        return [
+            ('id',
+             'in',
+             self.search(
+                [('product_id.qty_available', operator, operand)]).ids)]
