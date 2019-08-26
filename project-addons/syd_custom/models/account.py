2d75ab81f42adc7317faa5584bedd41e1240cd18
Omar (Comunitea) <omar@comunitea.com>
2019-08-26 21:15:56 +0200

    [IMP] syd_custom: Pequeñas mejoras en el análisis de facturas

--- project-addons/syd_custom/models/account.py
+++ project-addons/syd_custom/models/account.py
@@ -59,3 +59,29 @@ class AccountInvoice(models.Model):
         if payment_orders:
             action['domain'] = [('id', 'in', payment_orders.ids)]
         return action
+
+
+class AccountInvoiceReport(models.Model):
+
+    _inherit = 'account.invoice.report'
+
+    payment_mode_id = fields.Many2one('account.payment.mode', 'Payment mode')
+    number = fields.Char('Number')
+
+    def _select(self):
+        select_str = super()._select()
+        select_str += ', sub.payment_mode_id as payment_mode_id,' \
+                      ' sub.number as number'
+        return select_str
+
+    def _sub_select(self):
+        select_str = super()._sub_select()
+        select_str += ', ai.payment_mode_id,' \
+                      ' ai.number'
+        return select_str
+
+    def _group_by(self):
+        group_by_str = super()._group_by()
+        group_by_str += ', ai.payment_mode_id, ai.number'
+
+        return group_by_str
