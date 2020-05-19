e369efd81bf7e9bc360c89bef13f24a1e88cf664
ruben <ruben@comunitea.com>
2020-05-19 19:47:43 +0200

    [FIX] Btn and links colors by variables

--- project-addons/theme_sande/__manifest__.py
+++ project-addons/theme_sande/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Theme Sande y Díaz',
-    'version': '11.0.0.0.1',
+    'version': '11.0.0.0.2',
     'summary': 'Theme customization for Sande y Díaz website',
     'description': '',
     'category': 'Theme',
--- project-addons/theme_sande/doc/changelog.rst
+++ project-addons/theme_sande/doc/changelog.rst
@@ -1,3 +1,7 @@
+`11.0.0.0.2`
+------------
+- **[FIX]**  Btn and links colors by variables
+
 `11.0.0.0.1`
 ------------
 - **[FIX]**  Body content colors by variables
